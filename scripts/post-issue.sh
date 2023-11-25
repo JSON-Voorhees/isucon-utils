e08f7037dc39df0b410370a70a11e8bedcee6b45
youdofoo <fukushun1994@gmail.com>
2023-11-25 12:58:47 +0900

    add dns db

--- scripts/post-issue.sh
+++ scripts/post-issue.sh
@@ -2,6 +2,7 @@ SSH_OPTION=""
 SSH_USER="isucon"
 ALP_HOST="35.75.241.171"
 PT_HOST="35.79.126.247"
+PT_HOST2="52.196.206.96"
 DIR="/home/isucon/webapp"
 
 source ./github-setting.sh
@@ -9,6 +10,7 @@ source ./github-setting.sh
 ssh ${SSH_OPTION} ${SSH_USER}@${ALP_HOST} "cd ${DIR} && git branch --show-current && git rev-parse HEAD" > /tmp/commit.txt
 ssh ${SSH_OPTION} ${SSH_USER}@${ALP_HOST} "cd ${DIR} && make alp" > /tmp/alp.txt
 ssh ${SSH_OPTION} ${SSH_USER}@${PT_HOST}  "cd ${DIR} && make pt" > /tmp/pt.txt
+ssh ${SSH_OPTION} ${SSH_USER}@${PT_HOST2}  "cd ${DIR} && make pt" > /tmp/pt2.txt
 
 BODY=$(cat <<EOS
 # score: ${1}
@@ -25,12 +27,26 @@ $(cat /tmp/alp.txt)
 
 </details>
 
-<details><summary>pt-query-digest</summary>
+<details><summary>pt-query-digest (isupipe)</summary>
 
 \`\`\`
 
 
 $(cat /tmp/pt.txt | while read line
+do
+echo "${line}"
+done
+)
+
+\`\`\`
+</details>
+
+<details><summary>pt-query-digest (isudns)</summary>
+
+\`\`\`
+
+
+$(cat /tmp/pt2.txt | while read line
 do
 echo "${line}"
 done
