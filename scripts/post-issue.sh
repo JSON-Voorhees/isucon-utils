3637e2eda93bda2edcaf3adf71b7ee2eec712c45
youdofoo <fukushun1994@gmail.com>
2023-11-25 16:32:08 +0900

    fix

--- scripts/post-issue.sh
+++ scripts/post-issue.sh
@@ -7,10 +7,10 @@ DIR="/home/isucon/webapp"
 
 source ./github-setting.sh
 
-ssh ${SSH_OPTION} ${SSH_USER}@${ALP_HOST} "cd ${DIR} && git branch --show-current && git rev-parse HEAD" > /tmp/commit.txt
-ssh ${SSH_OPTION} ${SSH_USER}@${ALP_HOST} "cd ${DIR} && make alp" > /tmp/alp.txt
-ssh ${SSH_OPTION} ${SSH_USER}@${PT_HOST}  "cd ${DIR} && make pt" > /tmp/pt.txt
-#ssh ${SSH_OPTION} ${SSH_USER}@${PT_HOST2}  "cd ${DIR} && make pt" > /tmp/pt2.txt
+# ssh ${SSH_OPTION} ${SSH_USER}@${ALP_HOST} "cd ${DIR} && git branch --show-current && git rev-parse HEAD" > /tmp/commit.txt
+# ssh ${SSH_OPTION} ${SSH_USER}@${ALP_HOST} "cd ${DIR} && make alp" > /tmp/alp.txt
+# ssh ${SSH_OPTION} ${SSH_USER}@${PT_HOST}  "cd ${DIR} && make pt" > /tmp/pt.txt
+# ssh ${SSH_OPTION} ${SSH_USER}@${PT_HOST2}  "cd ${DIR} && make pt" > /tmp/pt2.txt
 
 BODY=$(cat <<EOS
 # score: ${1}
@@ -32,7 +32,7 @@ $(cat /tmp/alp.txt)
 \`\`\`
 
 
-$(cat /tmp/pt.txt | while read line
+$(cat /tmp/pt.txt | head -n 300 | while read line
 do
 echo "${line}"
 done
@@ -41,6 +41,20 @@ done
 \`\`\`
 </details>
 
+<details><summary>pt-query-digest (isudns)</summary>
+
+\`\`\`
+
+
+$(cat /tmp/pt2.txt | head -n 300 | while read line
+do
+echo "${line}" | cut -c 100
+done
+)
+
+\`\`\`
+
+</details>
 EOS
 )
 jq -n --arg body "${BODY}" '{body: $body}' > /tmp/comment.json
