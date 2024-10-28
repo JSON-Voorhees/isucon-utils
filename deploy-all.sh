03a6e06b66a84170911a7613f772d2616b9a13e4
youdofoo <fukushun1994@gmail.com>
2024-10-28 23:22:46 +0900

    fix deploy-all

--- deploy-all.sh
+++ deploy-all.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
+B=${1:-main}
+
 SERVERS="isu1 isu2 isu3"
-WEBAPP_DIR=~/webapp
+WEBAPP_DIR=/home/isucon/webapp
 
 for s in ${SERVERS}; do
-    ssh ${s} "cd ${WEBAPP_DIR}; make deploy" &
+    ssh ${s} "cd ${WEBAPP_DIR}; make deploy B=${B}" &
 done
 
 wait
